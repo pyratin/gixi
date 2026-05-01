3180e07c43906b206126c50729061329d5cdc7bd
pyratin <pyratin@hotmail.com>
2026-05-01 11:05:54 +0530

    backgroundLoad

--- source/browser/index.js
+++ source/browser/index.js
@@ -1,11 +1,5 @@
 import * as pixiJs from 'pixi.js';
-import {
-  Assets,
-  Application,
-  Container,
-  Graphics,
-  AnimatedSprite
-} from 'pixi.js';
+import { Assets, Application, Container, Graphics, Sprite } from 'pixi.js';
 import gsap from 'gsap';
 import gsapPixiPlugin from 'gsap/PixiPlugin';
 
@@ -15,9 +9,16 @@ gsap.registerPlugin(gsapPixiPlugin);
 
 gsapPixiPlugin.registerPIXI(pixiJs);
 
-const textureCollection = await Assets.load('/asset/sprite/fighter.json').then(
-  ({ textures }) => Object.values(textures)
+const assetAliasCollection = ['flowerTop', 'eggHead'];
+
+Assets.add(
+  assetAliasCollection.map((alias) => ({
+    alias,
+    src: `/asset/sprite/${alias}.png`
+  }))
 );
+
+Assets.backgroundLoad(assetAliasCollection);
 
 const application = new Application();
 
@@ -26,10 +27,10 @@ await application.init({ resizeTo: window, backgroundColor: 0x1099bb });
 document.body.appendChild(application.canvas);
 
 const container = new Container({
-  ...(() => {
-    const { width, height } = application.screen;
+  position: (() => {
+    const { screen: { width = 0, height = 0 } = {} } = application;
 
-    return { position: { x: width / 2, y: height / 2 } };
+    return { x: width / 2, y: height / 2 };
   })()
 });
 
@@ -38,7 +39,9 @@ application.stage.addChild(container);
 const graphics = new Graphics()
   .rect(
     ...(() => {
-      const { width, height } = application.screen;
+      const {
+        screen: { width, height }
+      } = application;
 
       return /** @type {const} */ ([-width / 2, -height / 2, width, height]);
     })()
@@ -47,34 +50,45 @@ const graphics = new Graphics()
 
 container.addChild(graphics);
 
-const _container = new Container();
+let assetAliasIndexActive = 0;
+
+const _container = new Container({
+  eventMode: 'static',
+  cursor: 'pointer',
+  onpointertap: async (event) => {
+    assetAliasIndexActive = !assetAliasIndexActive ? 1 : 0;
+
+    Object.assign(
+      event.target.getChildByLabel('graphics'),
+      /** @type {pixiJs.SpriteOptions} */ ({
+        texture: await Assets.load(assetAliasCollection[assetAliasIndexActive])
+      })
+    );
+  }
+});
 
 container.addChild(_container);
 
-const animatedSprite = new AnimatedSprite({
-  textures: textureCollection,
-  anchor: 0.5,
-  animationSpeed: 0.5
+Assets.load(assetAliasCollection[assetAliasIndexActive]).then((texture) => {
+  const sprite = new Sprite({ label: 'graphics', texture, anchor: 0.5 });
+
+  _container.addChild(sprite);
+
+  const _graphics = new Graphics()
+    .rect(
+      ...(() => {
+        const { width, height } = _container;
+
+        return /** @type {const} */ ([-width / 2, -height / 2, width, height]);
+      })()
+    )
+    .stroke({ alignment: 1, width: 1, color: 0x000000 });
+
+  _container.addChild(_graphics);
 });
 
-_container.addChild(animatedSprite);
-
-animatedSprite.play();
-
-const _graphics = new Graphics()
-  .rect(
-    ...(() => {
-      const { width, height } = _container.getLocalBounds();
-
-      return /** @type {const} */ ([-width / 2, -height / 2, width, height]);
-    })()
-  )
-  .stroke({ alignment: 1, width: 1, color: 0x000000 });
-
-_container.addChild(_graphics);
-
 gsap.to(_container, {
-  pixi: { angle: 360 },
+  pixi: { angle: 0 },
   duration: 4,
   repeat: -1,
   ease: 'none'
