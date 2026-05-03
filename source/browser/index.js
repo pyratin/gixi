1a1abe2a14d4b6e3f78d65e1eb4252def8a2ed5d
pyratin <pyratin@hotmail.com>
2026-05-03 07:26:11 +0530

    gsap 2

--- source/browser/index.js
+++ source/browser/index.js
@@ -3,11 +3,11 @@ import { Application, Graphics } from 'pixi.js';
 import { DropShadowFilter } from 'pixi-filters';
 import gsap from 'gsap';
 import gsapPixiPlugin from 'gsap/PixiPlugin';
-import gsapPhysics2D from 'gsap/Physics2DPlugin';
+import gsapPhysics2DPlugin from 'gsap/Physics2DPlugin';
 
 import '#browser/index.scss';
 
-gsap.registerPlugin(gsapPixiPlugin, gsapPhysics2D);
+gsap.registerPlugin(gsapPixiPlugin, gsapPhysics2DPlugin);
 gsapPixiPlugin.registerPIXI(pixiJs);
 
 const application = new Application();
@@ -21,37 +21,36 @@ Object.assign(
   /** @type {pixiJs.ContainerOptions} */ ({
     eventMode: 'static',
     cursor: 'pointer',
-    hitArea: application.screen
+    hitArea: application.screen,
+    onpointertap: ({ clientX, clientY }) => {
+      Array.from({ length: gsap.utils.random(15, 30, 1) }).map(() => {
+        const graphics = new Graphics({
+          position: { x: clientX, y: clientY },
+          scale: 0,
+          filters: [new DropShadowFilter()]
+        })
+          .circle(0, 0, gsap.utils.random(20, 40))
+          .fill({ color: 0xffffff });
+
+        application.stage.addChild(graphics);
+
+        gsap
+          .timeline()
+          .to(graphics, {
+            pixi: { scale: gsap.utils.random(0.25, 1) },
+            duration: 0.025,
+            ease: 'power3.out'
+          })
+          .to(graphics, {
+            physics2D: {
+              velocity: gsap.utils.random(500, 1000),
+              gravity: 1500,
+              angle: gsap.utils.random(0, 360)
+            },
+            duration: 2,
+            ease: 'none'
+          });
+      });
+    }
   })
 );
-
-application.stage.on('pointertap', ({ clientX, clientY }) => {
-  Array.from({ length: gsap.utils.random(15, 30, 1) }).map(() => {
-    const graphics = new Graphics({
-      position: { x: clientX, y: clientY },
-      scale: 0,
-      filters: [new DropShadowFilter()]
-    })
-      .circle(0, 0, gsap.utils.random(20, 40))
-      .fill({ color: 0xffffff });
-
-    application.stage.addChild(graphics);
-
-    gsap
-      .timeline({ onComplete: () => graphics.destroy() })
-      .to(graphics, {
-        pixi: { scale: gsap.utils.random(0.25, 1) },
-        duration: 0.02,
-        ease: 'power3.out'
-      })
-      .to(graphics, {
-        physics2D: {
-          velocity: gsap.utils.random(500, 1000),
-          gravity: 1500,
-          angle: gsap.utils.random(0, 360)
-        },
-        duration: 2,
-        ease: 'none'
-      });
-  });
-});
