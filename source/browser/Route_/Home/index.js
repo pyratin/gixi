b7c129c4e3b05477a5e2aab3bf1463d0e3997132
pyratin <pyratin@hotmail.com>
2026-05-05 08:44:51 +0530

    manifest

--- source/browser/Route_/Home/index.js
+++ source/browser/Route_/Home/index.js
@@ -7,37 +7,57 @@ import { useExtend } from '@pixi/react';
 import Application_ from './Component/Application_';
 import style from './index.module.scss';
 
-const assetAliasCollection = ['flowerTop', 'eggHead'];
+/** @type {[string, string[]][]} */
+const bundleDefinitionCollection = [
+  ['start-screen', ['flowerTop']],
+  ['game-screen', ['eggHead']]
+];
 
-Assets.add(
-  assetAliasCollection.map((alias) => ({
-    alias,
-    src: `/asset/sprite/${alias}.png`
-  }))
-);
+Assets.init({
+  manifest: {
+    bundles: bundleDefinitionCollection.map(([name, assetAliasCollection]) => ({
+      name,
+      assets: assetAliasCollection.map((alias) => ({
+        alias,
+        src: `/asset/sprite/${alias}.png`
+      }))
+    }))
+  }
+});
 
-Assets.backgroundLoad(assetAliasCollection);
+Assets.backgroundLoadBundle(bundleDefinitionCollection.map(([name]) => name));
 
-const LayoutContainer__ = ({
-  assetAliasIndexActive,
-  assetAliasIndexActiveSet
+const LayoutContainer_ = ({
+  bundleDefinitionIndexActive,
+  bundleDefinitionIndexActiveSet
 }) => {
   useExtend({ LayoutContainer, Sprite });
 
   const [texture, textureSet] = useState(undefined);
 
   useEffect(() => {
-    Assets.load(assetAliasCollection[assetAliasIndexActive]).then(textureSet);
-  }, [assetAliasIndexActive]);
+    const [name, [alias]] =
+      bundleDefinitionCollection[bundleDefinitionIndexActive];
+
+    Assets.loadBundle(name).then((bundleObject) =>
+      textureSet(bundleObject[alias])
+    );
+  }, [bundleDefinitionIndexActive]);
 
   return (
     <pixiLayoutContainer
-      layout={{ padding: 20, borderWidth: 1, borderColor: 0x000000 }}
+      layout={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        borderWidth: 1,
+        borderColor: 0x000000
+      }}
       eventMode='static'
       cursor='pointer'
       onPointerTap={() =>
-        assetAliasIndexActiveSet((assetAliasIndexActive = 0) =>
-          !assetAliasIndexActive ? 1 : 0
+        bundleDefinitionIndexActiveSet((bundleDefinitionIndexActive = 0) =>
+          !bundleDefinitionIndexActive ? 1 : 0
         )
       }
     >
@@ -46,35 +66,17 @@ const LayoutContainer__ = ({
   );
 };
 
-const LayoutContainer_ = () => {
-  useExtend({ LayoutContainer });
-
-  const [assetAliasIndexActive, assetAliasIndexActiveSet] = useState(0);
-
-  return (
-    <pixiLayoutContainer
-      layout={{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 10,
-        borderColor: 0x000000
-      }}
-    >
-      <LayoutContainer__
-        assetAliasIndexActive={assetAliasIndexActive}
-        assetAliasIndexActiveSet={assetAliasIndexActiveSet}
-      />
-    </pixiLayoutContainer>
-  );
-};
-
 const Home = () => {
+  const [bundleDefinitionIndexActive, bundleDefinitionIndexActiveSet] =
+    useState(0);
+
   return (
     <div className={['Home', style.Home].join(' ')}>
       <Application_ backgroundColor={0x1099bb}>
-        <LayoutContainer_ />
+        <LayoutContainer_
+          bundleDefinitionIndexActive={bundleDefinitionIndexActive}
+          bundleDefinitionIndexActiveSet={bundleDefinitionIndexActiveSet}
+        />
       </Application_>
     </div>
   );
